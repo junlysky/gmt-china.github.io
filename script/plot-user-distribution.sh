e203a03fc6e87d00073ca6c014a85a6e5e03274b
Dongdong Tian <seisman.info@gmail.com>
2016-04-04 22:02:40 +0800

    更新绘图脚本

--- script/plot-user-distribution.sh
+++ script/plot-user-distribution.sh
@@ -2,12 +2,15 @@
 file=user-distribution.ps
 title=GMT中文用户分布
 
-gmt pscoast -JJ155/27c -R0/360/-60/80 -X1.5c -Y3c -A33000 -Dc -Gskyblue -K > $file
-gmt psxy -J -R -Sc0.2c -Gorange -W0.1p,black,solid -: -O >> $file << EOF
+gmt pscoast -JJ155/27c -R0/360/-62/80 -X1.5c -Y3c -A33000 -Dc -Gskyblue -K > $file
+gmt psxy -J -R -Sc0.2c -Gorange -W0.1p,black,solid -: -K -O >> $file << EOF
 39.9 116.3 北京
 31.7 117.2 合肥
 30.6 114.3 武汉
 25.2 110.2 桂林
+EOF
+gmt pstext -J -R -O -F+cBL -D0.5c/1c >> $file << EOF
+Updated at `date +'%m/%d/%Y'`
 EOF
 sed -i 's/^%%Title:.*/%%Title: '"$title/" $file
 gmt psconvert -A -P -Tg $file
